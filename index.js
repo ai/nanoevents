13e0687464081c5c1ede86458273a1a55b52a1e8
Andrey Sitnik <andrey@sitnik.ru>
2016-09-21 13:37:09 +1000

    Add base API

--- index.js
+++ index.js
@@ -1,7 +1,148 @@
+/**
+ * Interface for event subscription.
+ *
+ * @example
+ * class Ticker {
+ *   constructor() {
+ *     this.events = new EventEmitter5()
+ *   }
+ *   on() {
+ *     return this.events.on.apple(this.events, arguments)
+ *   }
+ *   once() {
+ *     return this.events.once.apple(this.events, arguments)
+ *   }
+ *   tick() {
+ *     this.events.emit('tick')
+ *   }
+ * }
+ *
+ * @class
+ */
 function EventEmitter5 () {
-
+  this.listeners = { }
 }
 
-EventEmitter5.prototype = { }
+function add (emitter, event, callback) {
+  var isSubscribed = true
+  var listener = { fn: callback }
+
+  listener.rm = function () {
+    if (!isSubscribed) return
+    isSubscribed = false
+    var listeners = emitter.listeners[event]
+    listeners.splice(listeners.indexOf(listener), 1)
+  }
+
+  if (emitter.listeners[event]) {
+    emitter.listeners[event].push(listener)
+  } else {
+    emitter.listeners[event] = [listener]
+  }
+  return listener
+}
+
+EventEmitter5.prototype = {
+
+  /**
+   * Add a listener for a given event.
+   *
+   * @param {string} event The event name.
+   * @param {function} callback The listener function.
+   *
+   * @return {function} Unbind listener from event
+   *
+   * @example
+   * var unbind = ee.on('tick', function () {
+   *   count += 1
+   * })
+   *
+   * disable () {
+   *   unbind()
+   * }
+   */
+  on: function on (event, callback) {
+    var listener = add(this, event, callback)
+    return listener.rm
+  },
+
+  /**
+   * Add a one-time listener for a given event.
+   *
+   * @param {string} event The event name.
+   * @param {function} callback The listener function.
+   *
+   * @return {function} Unbind listener from event
+   *
+   * @example
+   * var unbind = ee.once('tick', function () {
+   *   works = true
+   * })
+   */
+  once: function once (event, callback) {
+    var listener = add(this, event, callback)
+    listener.once = true
+    return listener.rm
+  },
+
+  /**
+   * Calls each of the listeners registered for a given event.
+   *
+   * @param {string} event The event name.
+   *
+   * @returns {boolean} `true` if the event had listeners, else `false`.
+   *
+   * @example
+   * ee.emit('tick', tickType, tickMode)
+   */
+  emit: function emit (event, a1, a2, a3, a4, a5, a6) {
+    var listeners = this.listeners[event]
+    if (!listeners || listeners.length === 0) return false
+
+    var args = arguments
+    listeners.forEach(function (i) {
+      args[0] = i
+      this.call.apply(this, args)
+    }, this)
+
+    return true
+  },
+
+  /**
+   * Execute listener callback.
+   *
+   * @param {object} listener Listener object
+   *
+   * @return {any} callback result
+   *
+   * @example
+   * for ( listener of ee.listeners['tick'] ) {
+   *   if ( ee.call(listener, tickType, tickMode) === false ) {
+   *     break
+   *   }
+   * }
+   */
+  call: function call (listener, a1, a2, a3) {
+    if (listener.once) listener.rm()
+    switch (arguments.length) {
+      case 1:
+        listener.fn.call(this)
+        break
+      case 2:
+        listener.fn.call(this, a1)
+        break
+      case 3:
+        listener.fn.call(this, a1, a2)
+        break
+      default:
+        var args = new Array(arguments.length - 1)
+        for (var i = 1; i < arguments.length; i++) {
+          args[i - 1] = arguments[i]
+        }
+        listener.fn.apply(this, args)
+    }
+  }
+
+}
 
 module.exports = EventEmitter5
--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,113 @@
 var EventEmitter5 = require('../')
 
 it('is a class', function () {
-  var instance = new EventEmitter5()
+  var ee = new EventEmitter5()
   expect(typeof EventEmitter5).toEqual('function')
-  expect(instance instanceof EventEmitter5).toBeTruthy()
+  expect(ee instanceof EventEmitter5).toBeTruthy()
+})
+
+it('is empty from the beggining', function () {
+  var ee = new EventEmitter5()
+  expect(ee.listeners).toEqual({ })
+})
+
+it('adds listeners', function () {
+  var ee = new EventEmitter5()
+
+  ee.on('one', function () { })
+  ee.on('two', function () { })
+  ee.once('two', function () { })
+
+  expect(Object.keys(ee.listeners)).toEqual(['one', 'two'])
+  expect(ee.listeners.one.length).toEqual(1)
+  expect(ee.listeners.two.length).toEqual(2)
+})
+
+it('calls listener', function () {
+  var ee = new EventEmitter5()
+  var calls = []
+  ee.on('event', function () {
+    calls.push(Array.prototype.slice.call(arguments))
+  })
+
+  ee.emit('event')
+  ee.emit('event', 11)
+  ee.emit('event', 21, 22)
+  ee.emit('event', 31, 32, 33)
+  ee.emit('event', 41, 42, 43, 44)
+
+  expect(calls).toEqual([[], [11], [21, 22], [31, 32, 33], [41, 42, 43, 44]])
+})
+
+it('unbinds listener', function () {
+  var ee = new EventEmitter5()
+
+  var calls1 = []
+  var unbind = ee.on('event', function (a) {
+    calls1.push(a)
+  })
+
+  var calls2 = []
+  ee.on('event', function (a) {
+    calls2.push(a)
+  })
+
+  ee.emit('event', 1)
+  unbind()
+  ee.emit('event', 2)
+
+  expect(calls1).toEqual([1])
+  expect(calls2).toEqual([1, 2])
+})
+
+it('does not fall on multiple unbind', function () {
+  var ee = new EventEmitter5()
+  var unbind = ee.on('event', function () { })
+  unbind()
+  unbind()
+})
+
+it('calls listener once on request', function () {
+  var ee = new EventEmitter5()
+  var calls = []
+  ee.once('event', function (a) {
+    calls.push(a)
+  })
+
+  ee.emit('event', 1)
+  ee.emit('event', 2)
+
+  expect(calls).toEqual([1])
+})
+
+it('calls event manually', function () {
+  var ee = new EventEmitter5()
+
+  var calls1 = []
+  ee.on('event', function (a) {
+    calls1.push(a)
+  })
+
+  var calls2 = []
+  ee.on('event', function (a) {
+    calls2.push(a)
+  })
+
+  ee.call(ee.listeners.event[0], 1)
+
+  expect(calls1).toEqual([1])
+  expect(calls2).toEqual([])
+})
+
+it('removes once listener on manually call', function () {
+  var ee = new EventEmitter5()
+  var calls = []
+  ee.once('event', function (a) {
+    calls.push(a)
+  })
+
+  ee.call(ee.listeners.event[0], 1)
+
+  expect(calls).toEqual([1])
+  expect(ee.listeners.event.length).toEqual(0)
 })
