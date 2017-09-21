cf9e5c1431e84f58bd81b38a11fe003d0662f3b6
Andrey Sitnik <andrey@sitnik.ru>
2017-09-22 02:43:35 +1000

    Revert unbind-all compression

--- unbind-all.js
+++ unbind-all.js
@@ -1,14 +1,15 @@
-module.exports =
-  /**
-   * Removes all listeners.
-   *
-   * @alias unbindAll
-   * @param {NanoEvents} emitter NanoEvents instance.
-   * @returns {undefined}
-   *
-   * @example
-   * unbindAll(emitter)
-   */
-  function unbindAll (emitter) {
-    emitter.events = { }
-  }
+/**
+ * Removes all listeners.
+ *
+ * @param {NanoEvents} emitter NanoEvents instance.
+ *
+ * @returns {undefined}
+ *
+ * @example
+ * unbindAll(emitter)
+ */
+function unbindAll (emitter) {
+  emitter.events = { }
+}
+
+module.exports = unbindAll
