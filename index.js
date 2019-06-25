0b31412b81c32f6a79da9bb6a6f1eb0e6e41761a
Andrey Gurtovoy <iyntx@ya.ru>
2019-06-25 15:00:45 +0300

    unnecessary 'filter' replaced by 'forEach'

--- index.js
+++ index.js
@@ -51,7 +51,7 @@
   emit: function emit (event) {
     var args = [].slice.call(arguments, 1)
     // Array.prototype.call() returns empty array if context is not array-like
-    ;[].slice.call(this.events[event] || []).filter(function (i) {
+    ;[].slice.call(this.events[event] || []).forEach(function (i) {
       i.apply(null, args)
     })
   },
