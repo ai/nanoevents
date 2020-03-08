d544a669bce219e354ba40b3d8c98c576bc22763
Bogdan Chadkin <trysound@yandex.ru>
2020-03-08 12:23:38 +0300

    Drop ts namespace
    Ref https://github.com/ai/nanoevents/commit/815d81d147754cc27d56a41ff08278c00ba6ce94#r37694803

--- index.d.ts
+++ index.d.ts
@@ -1,65 +1,63 @@
-export namespace createNanoEvents {
-  interface EventsMap {
-    [event: string]: any
-  }
+interface EventsMap {
+  [event: string]: any
+}
 
-  interface DefaultEvents extends EventsMap {
-    [event: string]: (...args: any) => void
-  }
+interface DefaultEvents extends EventsMap {
+  [event: string]: (...args: any) => void
+}
 
-  class Emitter<Events extends EventsMap> {
-    /**
-     * Event names in keys and arrays with listeners in values.
-     *
-     * ```js
-     * emitter1.events = emitter2.events
-     * emitter2.events = { }
-     * ```
-     */
-    events: Partial<{ [E in keyof Events]: Events[E][] }>
+export declare class Emitter<Events extends EventsMap> {
+  /**
+   * Event names in keys and arrays with listeners in values.
+   *
+   * ```js
+   * emitter1.events = emitter2.events
+   * emitter2.events = { }
+   * ```
+   */
+  events: Partial<{ [E in keyof Events]: Events[E][] }>
 
-    /**
-     * Add a listener for a given event.
-     *
-     * ```js
-     * const unbind = ee.on('tick', (tickType, tickDuration) => {
-     *   count += 1
-     * })
-     *
-     * disable () {
-     *   unbind()
-     * }
-     * ```
-     *
-     * @param event The event name.
-     * @param cb The listener function.
-     * @returns Unbind listener from event.
-     */
-    on <K extends keyof Events>(this: this, event: K, cb: Events[K]): () => void
+  /**
+   * Add a listener for a given event.
+   *
+   * ```js
+   * const unbind = ee.on('tick', (tickType, tickDuration) => {
+   *   count += 1
+   * })
+   *
+   * disable () {
+   *   unbind()
+   * }
+   * ```
+   *
+   * @param event The event name.
+   * @param cb The listener function.
+   * @returns Unbind listener from event.
+   */
+  on <K extends keyof Events>(this: this, event: K, cb: Events[K]): () => void
 
-    /**
-     * Calls each of the listeners registered for a given event.
-     *
-     * ```js
-     * ee.emit('tick', tickType, tickDuration)
-     * ```
-     *
-     * @param event The event name.
-     * @param args The arguments for listeners.
-     */
-    emit <K extends keyof Events>(
-      this: this,
-      event: K,
-      ...args: Parameters<Events[K]>
-    ): void
-  }
+  /**
+   * Calls each of the listeners registered for a given event.
+   *
+   * ```js
+   * ee.emit('tick', tickType, tickDuration)
+   * ```
+   *
+   * @param event The event name.
+   * @param args The arguments for listeners.
+   */
+  emit <K extends keyof Events>(
+    this: this,
+    event: K,
+    ...args: Parameters<Events[K]>
+  ): void
 }
 
 /**
  * Create event emitter.
  *
  * ```js
- * import createNanoEvents from 'nanoevents'
+ * import { createNanoEvents } from 'nanoevents'
  *
  * class Ticker {
  *   constructor() {
@@ -75,5 +73,5 @@ export namespace createNanoEvents {
  * ```
  */
 export function createNanoEvents<
-  Events extends createNanoEvents.EventsMap = createNanoEvents.DefaultEvents
-> (): createNanoEvents.Emitter<Events>
+  Events extends EventsMap = DefaultEvents
+> (): Emitter<Events>
