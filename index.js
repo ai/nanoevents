296a9c95740e5dd5cc6adde3e2dcc41fc3ec49a8
Mikhail Nasyrov <streambot@gmail.com>
2021-02-16 10:33:43 +0700

    fix: "unbind" callback fails after removing all (#52)
    ai/nanoevents#51

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let createNanoEvents = () => ({
   events: {},
   emit (event, ...args) {
-    for (let i of this.events[event] || []) {
-      i(...args)
-    }
+    ;(this.events[event] || []).forEach(i => i(...args))
   },
   on (event, cb) {
     ;(this.events[event] = this.events[event] || []).push(cb)
-    return () => (this.events[event] = this.events[event].filter(i => i !== cb))
+    return () =>
+      (this.events[event] = (this.events[event] || []).filter(i => i !== cb))
   }
 })
 
