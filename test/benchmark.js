fbda24268a9008a7aec439105be77b17f7a346e3
Andrey Sitnik <andrey@sitnik.ru>
2022-04-09 10:41:50 +0200

    Add benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import benchmark from 'benchmark'
+
+import { createNanoEvents } from '../index.js'
+
+let suite = new benchmark.Suite()
+
+function formatNumber(number) {
+  return String(number)
+    .replace(/\d{3}$/, ',$&')
+    .replace(/^(\d|\d\d)(\d{3},)/, '$1,$2')
+}
+
+let counter = 0
+let events = createNanoEvents()
+events.on('increase', add => {
+  counter += add
+})
+
+suite
+  .add('emit', () => {
+    events.emit('increase', 1)
+  })
+  .add('on', () => {
+    let unbind = events.on('print', () => {
+      process.stdout.write(counter)
+    })
+    unbind()
+  })
+  .on('cycle', event => {
+    let name = event.target.name.padEnd('emit  '.length)
+    let hz = formatNumber(event.target.hz.toFixed(0)).padStart(9)
+    process.stdout.write(`${name}${hz} ops/sec\n`)
+  })
+  .on('error', event => {
+    process.stderr.write(event.target.error.toString() + '\n')
+    process.exit(1)
+  })
+  .run()
