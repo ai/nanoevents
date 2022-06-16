721a74d67f73c54c2fd8b0ff6d1b35d0cb79b45f
Andrey Sitnik <andrey@sitnik.ru>
2022-06-16 12:24:35 +0200

    Improve code readability and reduce size a little

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 export let createNanoEvents = () => ({
   events: {},
   emit(event, ...args) {
-    for (let i = 0, handlers = this.events[event] || [], length = handlers.length; i < length; i++) handlers[i](...args)
+    let callbacks = this.events[event] || []
+    for (let i = 0, length = callbacks.length; i < length; i++) {
+      callbacks[i](...args)
+    }
   },
   on(event, cb) {
     this.events[event]?.push(cb) || (this.events[event] = [cb])
-    return () => (this.events[event] = this.events[event]?.filter(i => cb !== i))
+    return () => {
+      this.events[event] = this.events[event]?.filter(i => cb !== i)
+    }
   }
 })
