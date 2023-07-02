ea99484f729d303b94245d66a0e85b1b9639ff89
Bribe <71581275+BribeFromTheHive@users.noreply.github.com>
2023-07-02 18:30:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 export let createNanoEvents = () => ({
   events: {},
   emit(event, ...args) {
-    for (const callback of (this.events[event] || [])) {
-      callback(...args)
-    }
+    this.events[event]?.forEach(cb => cb(...args));
   },
   on(event, cb) {
-    const events = (this.events[event] ||= [])
+    let events = this.events[event] ||= []
     events.push(cb)
     return () => {
-      const i = events.indexOf(cb)
+      let i = events.indexOf(cb)
       if (i > -1) events.splice(i, 1)
     }
   }
