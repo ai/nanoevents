debe0db71e6cf97b00d7eb3d371511fb9b5a33c0
Andrey Sitnik <andrey@sitnik.ru>
2023-11-23 06:59:41 +0100

    Prettierify code

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 export let createNanoEvents = () => ({
   emit(event, ...args) {
-    for (let i = 0, callbacks = this.events[event] || [], length = callbacks.length; i < length; i++) {
+    for (
+      let i = 0,
+        callbacks = this.events[event] || [],
+        length = callbacks.length;
+      i < length;
+      i++
+    ) {
       callbacks[i](...args)
     }
   },
   events: {},
   on(event, cb) {
-    (this.events[event]||=[]).push(cb);
+    ;(this.events[event] ||= []).push(cb)
     return () => {
       this.events[event] = this.events[event]?.filter(i => cb !== i)
     }
